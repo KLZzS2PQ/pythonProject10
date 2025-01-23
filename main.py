887bc6a1f1fca80b8bbde50cb24276f703f47cca
AndreyTech <lionlonn14@gmail.com>
2025-01-23 18:00:59 +0300

    pull data

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 import telebot
-token =  "7922935456:AAHMOi1tmtI_mT5QuK_CdgzlYCYAkWmq_NY"
+token = "7922935456:AAHMOi1tmtI_mT5QuK_CdgzlYCYAkWmq_NY"
 bot = telebot.TeleBot(token)
 
 @bot.message_handler(commands=["start"])
