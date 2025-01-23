72521bbf9764f48e264b8dd1a84bf9c9cbde892a
AndreyTech <lionlonn14@gmail.com>
2025-01-23 18:03:56 +0300

    test

--- main.py
+++ main.py
@@ -1,10 +1,12 @@
 import telebot
+import buttons
+import types
 token = "7922935456:AAHMOi1tmtI_mT5QuK_CdgzlYCYAkWmq_NY"
 bot = telebot.TeleBot(token)
 
 @bot.message_handler(commands=["start"])
 def start(message):
-    pass
+    buttons.button_bot(message, bot, types)
 
 
 bot.infinity_polling()
